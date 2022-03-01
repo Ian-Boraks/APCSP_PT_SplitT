97b742e6b64eb250bccc442e01eb9c9776f55310
Dubbyw <donald.paladino@s.stemk12.org>
2022-03-01 12:41:51 -0700

    Update timer_window.py

--- timer_window.py
+++ timer_window.py
@@ -11,16 +11,18 @@ import time
 def time_as_int():
     return int(round(time.time() * 100))
 
-
+user_times = []
 # ----------------  Create Form  ----------------
 sg.theme('Black')
 
 layout = [[sg.Text('')],
           [sg.Text('', size=(8, 2), font=('Helvetica', 20),
-                justification='center', key='text')],
+                justification='center', key='-TIMER-TEXT-'),
+           sg.Text(''), size=(8, 2), font=('Helvetica', 20), justification='center', key='-SPLIT-TEXT-'],
           [sg.Button('Pause', key='-RUN-PAUSE-', button_color=('white', '#001480')),
            sg.Button('Reset', button_color=('white', '#007339'), key='-RESET-'),
-           sg.Exit(button_color=('white', 'firebrick4'), key='Exit')]]
+           sg.Exit(button_color=('white', 'firebrick4'), key='Exit'), 
+           sg.Button('Split', key='-SPLIT-TIMER-', button_color=('white', '#ff0000'))]]
 
 window = sg.Window('Running Timer', layout,
                    no_titlebar=False,
@@ -30,7 +32,9 @@ window = sg.Window('Running Timer', layout,
                    element_padding=(0, 0),
                    finalize=True,
                    element_justification='c',
-                   right_click_menu=sg.MENU_RIGHT_CLICK_EDITME_EXIT)
+                   right_click_menu=sg.MENU_RIGHT_CLICK_EDITME_EXIT,
+                   size = (500, 500),
+                   resizable = True)
 
 current_time, paused_time, paused = 0, 0, False
 start_time = time_as_int()
@@ -56,12 +60,15 @@ while True:
             paused_time = time_as_int()
         else:
             start_time = start_time + time_as_int() - paused_time
+    elif event == '-SPLIT-TIMER-':
+        user_times.append(current_time)
+        window['-SPLIT-TEXT-'].update(user_times)
         # Change button's text
         window['-RUN-PAUSE-'].update('Run' if paused else 'Pause')
     elif event == 'Edit Me':
         sg.execute_editor(__file__)
     # --------- Display timer in window --------
-    window['text'].update('{:02d}:{:02d}.{:02d}'.format((current_time // 100) // 60,
+    window['-TIMER-TEXT-'].update('{:02d}:{:02d}.{:02d}'.format((current_time // 100) // 60,
                                                         (current_time // 100) % 60,
                                                         current_time % 100))
 window.close()
